29b982225c0cb13045f2fbb2f1e31cd87309b2a4
Tulipa Silva <tulipa@sim1897.simeparnet>
2016-01-19 17:29:00 -0200

    Radar_plots getting better
    Signed-off-by: Tulipa Silva <tulipa@sim1897.simeparnet>

--- radar_recipes/Radar_Plots/main.py
+++ radar_recipes/Radar_Plots/main.py
@@ -2,30 +2,70 @@
 
 import pyart
 import numpy as np
+import sys
 
 from ReadRadarStructure import ReadRadarStructure
 from Plot import Plot
 plot = Plot()
 
-#colocar strutura de argv
 
-filename = 'MC1-250--2016-01-11--00-00-03dBuZ.hdf5'
+"""
+Creat a jpg with the lower elevation from the radar structure.
+
+input:
+    infile: str
+        input filename
+
+    outfile: str
+        output filename
+
+output:
+    jpg that will be in images folder
+    
+Obs.:
+        Use:  python main.py <input filename> <output filename>
+"""
+
+try:
+    filename  = sys.argv[1]
+    figname = sys.argv[2]
+except:
+    print "Use:  python main.py <input filename> <output filename>"
+    print 'Exemplo: python main.py /discolocal/teste.hdf5 test_DBZH'
+    sys.exit()
+
+
 field = 'DBZH'
-#ler radar
 
-#colocar try except para plotar todos os tipos...ou quase
-radar = pyart.aux_io.read_odim_h5(filename)
+try:
+    radar = pyart.aux_io.read_odim_h5(filename)
+except:
+    try:
+        radar = pyart.aux_io.read_gamic(filename)
+    except:
+        try:
+            radar = pyart.io.read(filename)
+        except:
+            print "Unknown file format"
+            sys.exit()
 
-#verificar a questão do sweep a ser plotado...
-radar.fields[field]['data'].mask[np.where(radar.fields[field]['data'].data<=)]
+#try:
+#    radar.fields[field]['total_power'].mask[np.where(radar.fields[field]['total_power'].data<=15)]
+#except:
+#    radar.fields[field]['DBZH'].mask[np.where(radar.fields[field]['DBZH'].data<=15)]
 
 rs = ReadRadarStructure(radar)
-values = rs.mount_volume(radar, field)
+try:
+    field = 'DBZH'
+    values = rs.mount_volume(radar, field)
+except:
+    field = 'total_power'
+    values = rs.mount_volume(radar, field)
 
 #find lower elevation
 e = np.argmin(rs.fixed_angle)
 
 moment = 'DBZH'
-figname = 'imagem_DBZH'
+
 plot.PPI(rs, values, e, moment, figname)
 
